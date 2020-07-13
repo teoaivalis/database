aea35ec268491529fae9936020371d3a9df60f72
teoaivalis <68184383+teoaivalis@users.noreply.github.com>
2020-07-13 20:16:10 +0300

    Add files via upload

--- inserts.py
+++ inserts.py
@@ -3,7 +3,7 @@ from mysql_dbconfig import read_db_config
 from csv_reader import 	read_the_file
 
 
- def insert_store(store_id,square_meters, opening_hour, closing_hour, city, postal_code, street_name,street_number):
+def insert_store(store_id,square_meters, opening_hour, closing_hour, city, postal_code,street_name,street_number):
 	 query = "INSERT INTO store VALUES (%s,%s,%s,%s,%s,%s,%s,%s)"
 	 args = (store_id,square_meters, opening_hour, closing_hour, city, postal_code, street_name,street_number)
 	 try:
@@ -136,9 +136,12 @@ def insert_past_prices(date_of_change,past_price_of_product):
 
 
 
-def insert_contains(store_id_contains,type_id_contains):
-	query = "INSERT INTO contains VALUES(%s,%s)"
-	args = (store_id_contains,type_id_contains)
+
+
+
+def insert_product_type(type_id,name_of_type):
+	query = "INSERT INTO product_type VALUES(%s,%s,%s)"
+	args = (type_id,name_of_type)
 	try:
 		db_config = read_db_config()
 		conn = MySQLConnection(**db_config)
@@ -159,32 +162,18 @@ def insert_contains(store_id_contains,type_id_contains):
 		cursor.close()
 		conn.close()
 
-def insert_consists(product_id_consists,type_id_consists):
-    query = "INSERT INTO consists VALUES(%s,%s,%s)"
-	args = (product_id_consists,type_id_consists)
-	try:
-		db_config = read_db_config()
-		conn = MySQLConnection(**db_config)
 
-		cursor = conn.cursor()
-		cursor.execute(query, args)
 
-		if cursor.lastrowid:
-			print('done')
-		else:
-			print('last insert id not found')
 
-		conn.commit()
-	except Error as error:
-		print(error)
 
-	finally:
-		cursor.close()
-		conn.close()
 
-def insert_purchases(quantity,product_id_purchases,card_id_purchases,transaction_id_purchases):
+
+
+
+
+def insert_purchases(quantity,product_id_purchases,card_id_purchases,transaction_id_purchases,store_id_purchases):
 	query = "INSERT INTO purchases VALUES(%s,%s,%s,%s)"
-	args = (quantity,product_id_purchases,card_id_purchases,transaction_id_purchases)
+	args = (quantity,product_id_purchases,card_id_purchases,transaction_id_purchases,store_id_purchases)
 	try:
 		db_config = read_db_config()
 		conn = MySQLConnection(**db_config)
@@ -230,28 +219,6 @@ def insert_customer_points(total_cost_of_customer_purchases,card_id_points):
 
 
 
-def insert_product_type(type_id,name_of_type):
-	query = "INSERT INTO product_type VALUES(%s,%s,%s)"
-	args = (type_id,name_of_type)
-	try:
-		db_config = read_db_config()
-		conn = MySQLConnection(**db_config)
-
-		cursor = conn.cursor()
-		cursor.execute(query, args)
-
-		if cursor.lastrowid:
-			print('done')
-		else:
-			print('last insert id not found')
-
-		conn.commit()
-	except Error as error:
-		print(error)
-
-	finally:
-		cursor.close()
-		conn.close()
 
 def insert_transaction(transaction_id,time_of_transaction,date_of_transaction,means_of_payment,total_cost,total_amount_of_products):
 	query = "INSERT INTO transaction VALUES(%s,%s,%s,%s,%s,%s)"
@@ -301,30 +268,46 @@ def insert_customer(card_id,customers_city,customers_postal_code,customers_stree
     	conn.close()
 
 def main():
-	file_name = "customer.csv"
-	for x in read_the_file(file_name):
-		insert_customer(x[0],x[1],x[2],x[3],x[4],x[5],x[6],x[7],x[8],x[9],x[10],x[11],x[12])
-	file_name = "customerpoints.csv"
-	for x in read_the_file(file_name):
-		insert_customer_points(x[0],x[1])
-	file_name = "pastprices.csv"
-	for x in read_the_file(file_name):
-		insert_past_prices(x[0],x[1])
-    file_name = "purchases.csv"
-	for x in read_the_file(file_name):
-		insert_purchases(x[0],x[1],x[2],x[3])
-	file_name = "transaction.csv"
-	for x in read_the_file(file_name):
-		insert_transaction(x[0],x[1],x[2],x[3],x[4],x[5])
-    file_name = "store.csv"
+	file_name = "store.csv"
 	for x in read_the_file(file_name):
 		insert_store(x[0],x[1],x[2],x[3],x[4],x[5],x[6],x[7])
+
 	file_name = "has.csv"
 	for x in read_the_file(file_name):
 		insert_has(x[0],x[1],x[2],x[3])
+
+	file_name = "product.csv"
+	for x in read_the_file(file_name):
+		insert_has(x[0],x[1],x[2],x[3])
+
+	file_name = "usedtocost.csv"
+	for x in read_the_file(file_name):
+		insert_has(x[0],x[1])
+
+    #file_name = "pastprices.csv"
+	#for x in read_the_file(file_name):
+	#	insert_past_prices(x[0],x[1])
+
 	file_name = "type.csv"
 	for x in read_the_file(file_name):
 		insert_product_type(x[0],x[1])
+
+    #file_name = "purchases.csv"
+	#for x in read_the_file(file_name):
+	#	insert_purchases(x[0],x[1],x[2],x[3],x[4])
+
+	file_name = "customerpoints.csv"
+	for x in read_the_file(file_name):
+		insert_customer_points(x[0],x[1])
+
+	file_name = "transaction.csv"
+	for x in read_the_file(file_name):
+		insert_transaction(x[0],x[1],x[2],x[3],x[4],x[5])
+
+	file_name = "customer.csv"
+	for x in read_the_file(file_name):
+		insert_customer(x[0],x[1],x[2],x[3],x[4],x[5],x[6],x[7],x[8],x[9],x[10],x[11],x[12])
+
 
 if __name__ == '__main__':
 	main()
