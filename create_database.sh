793db25bd536ec879404e439414c6469946eb3f1
teoaivalis <68184383+teoaivalis@users.noreply.github.com>
2020-07-15 21:42:26 +0300

    Delete create_database.sh

--- create_database.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-mysql < database_delete.sql
-mysql < database_create.sql
-mysql < database_create_views.sql
-python3 mysql_insert.py
-mysql <  authored_create.sql
-mysql < triggers.sql
-echo 'triggers ok'
-mysql < database_insert_rest.sql
